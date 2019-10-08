371fb79dafa4ea4feccf07e2ba511b67bf58d624
Patrick Harrison <jahman@bellatlantic.net>
2019-10-08 16:50:45 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -38,11 +38,11 @@ end
 
 
 def can_get_the_first_item_in_the_database
-  Movie.first.title
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.title
+  Movie.last
 end
 
 def can_get_size_of_the_database
@@ -50,7 +50,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
- Movie.find_by(id: 1).title
+ Movie.find_by(id: 1)
 end
 
 def can_find_by_multiple_attributes
